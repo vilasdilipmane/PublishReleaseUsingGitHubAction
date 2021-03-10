a3c067be86ee89c6927e9f52e6f0cc2a56136a81
Vilas Dilip Mane <vilasma@cybage.com>
2021-03-10 18:49:08 +0530

    Update set-tag.sh

--- .github/scripts/set-tag.sh
+++ .github/scripts/set-tag.sh
@@ -2,12 +2,7 @@
 
 set -eo pipefail
 
-export buildNumber=10
-echo $buildNumber
-echo "Export buildNumber=10 Executed"
-export ARCH=arm
-echo "Export ARCH=arm Executed"
-
+export buildNumber=50
 echo "TagName=$buildNumber" >> $GITHUB_ENV
 
 
