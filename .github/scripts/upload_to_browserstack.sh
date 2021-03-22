c2d2a2a2d147170039de5f03f2a161980f610cc5
Vilas Dilip Mane <49619267+vilasdilipmane@users.noreply.github.com>
2021-03-22 13:07:41 +0530

    Update upload_to_browserstack.sh

--- .github/scripts/upload_to_browserstack.sh
+++ .github/scripts/upload_to_browserstack.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" -X POST "https://api-cloud.browserstack.com/app-live/upload" -F "file=/Users/runner/work/PublishReleaseUsingGitHubAction/PublishReleaseUsingGitHubAction/HelloGithuActions/TestApp.ipa"
+curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" -X POST "https://api-cloud.browserstack.com/app-live/upload" -F "file=@/Users/runner/work/PublishReleaseUsingGitHubAction/PublishReleaseUsingGitHubAction/HelloGithuActions/TestApp.ipa"
